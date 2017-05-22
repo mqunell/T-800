c97a12ce37088890f0ba74215f18bc6d7c517117
Matt Qunell <mqunell@gmail.com>
2017-05-22 02:23:17 -0700

    Remove "!hello", add "@T-800" detection

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     # Prevent the bot from responding to itself
-    if message.author == client.user:
-        return
+    if message.author != client.user:
 
-    # If the bot sees "!hello"
-    if message.content.startswith("!hello"):
+        # If someone tags the bot
+        if client.user in message.mention:
+            msg = "*TALK TO THE HAND, {0.author.mention}.*".format(message)
+            await client.send_message(message.channel, msg)
 
-        # Print "Prepare to be terminated, <user>"
-        msg = "Prepare to be terminated, {0.author.mention}".format(message)
-        await client.send_message(message.channel, msg)
-
-    # If the bot sees "fake news"
-    if "fake news" in message.content.lower():
-        msg = "*FAKE NEWS DETECTED. TERMINATING FAKE NEWS.*"
-        await client.send_message(message.channel, msg)
+        # If someone says "fake news"
+        if "fake news" in message.content.lower():
+            msg = "*FAKE NEWS DETECTED. TERMINATING FAKE NEWS.*"
+            await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
