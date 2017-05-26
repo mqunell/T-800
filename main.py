75b93e3b3ff0d4df8450a19e9044607661fef5ce
Matt Qunell <mqunell@gmail.com>
2017-05-26 14:23:09 -0700

    Add comments to "!remindme"

--- main.py
+++ main.py
@@ -12,25 +12,43 @@ async def on_message(message):
             msg = "*TALK TO THE HAND, {0.author.mention}.*".format(message)
             await client.send_message(message.channel, msg)
 
+
         # If someone says "fake news"
         if "fake news" in message.content.lower():
             msg = "*FAKE NEWS DETECTED. TERMINATING FAKE NEWS.*"
             await client.send_message(message.channel, msg)
 
+
         # If someone calls "!purge"
         if message.content.startswith("!purge"):
             await client.purge_from(message.channel)
 
+
         # If someone calls "!remindme"
         if message.content.startswith("!remindme"):
+
+            # Split the message into "!remindme", <minutes>, <message>
+            # Note: <message> could be multiple strings
             command = message.content.split(" ")
+
             error_msg = ""
 
+            # If valid number of args
             if len(command) >= 3:
+
+                # Attempt to parse <minutes>
                 minutes = parse_minutes(command[1])
 
+                # If valid number of <minutes>
                 if 1 <= minutes <= 60:
-                    await client.send_message(message.channel, "Author: <{0.author.mention}>, ".format(message) + "Minutes: <%d>, Message: <%s>" % (minutes, " ".join(command[2:])))
+
+                    # TESTING: Post the data to be timed
+                    msg  = "Author: <{0.author.mention}>, ".format(message)
+                    msg += "Minutes: <%d>" % minutes
+                    msg += "Message: <%s>" % " ".join(command[2:])
+
+                    await client.send_message(message.channel, msg)
+
                 else:
                     error_msg = "Invalid <minutes>; must be [1, 60]"
 
@@ -57,6 +75,7 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name="Human Simulator"))
 
 
+# Attempts to parse an int from a string
 def parse_minutes(str_input):
     minutes = -1
 
@@ -64,8 +83,6 @@ def parse_minutes(str_input):
         minutes = int(str_input)
     except:
         None
-
-    print("Returned minutes: %d" % minutes)
     
     return minutes
 
