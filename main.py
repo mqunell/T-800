042cc365b9e500c045e6f67abcebb488d1921ced
Matt Qunell <mqunell@gmail.com>
2017-05-26 19:32:54 -0700

    Restructure program, post "ONLINE" message in bot-testing channel

--- main.py
+++ main.py
@@ -2,8 +2,33 @@ import discord
 
 client = discord.Client()
 
+
+@client.event
+async def on_ready():
+    """
+    When the bot signs in
+    """
+
+    print("%s (ID: %s) logged in" % (client.user.name, client.user.id))
+    print("----------------------------------------")
+
+    # "bot-testing" chat channel id
+    channel_id = "316026333702651917"
+
+    # Post a message when the bot comes online
+    msg = "*T-800 ONLINE. YOUR CLOTHES, GIVE THEM TO ME. NOW.*"
+    await client.send_message(client.get_channel(channel_id), msg)
+
+    # Set the bot's "Playing" status
+    await client.change_presence(game=discord.Game(name="Human Simulator"))
+
+
 @client.event
 async def on_message(message):
+    """
+    When messages are posted
+    """
+
     # Prevent the bot from responding to itself
     if message.author != client.user:
 
@@ -12,78 +37,69 @@ async def on_message(message):
             msg = "*TALK TO THE HAND, {0.author.mention}.*".format(message)
             await client.send_message(message.channel, msg)
 
-
         # If someone says "fake news"
         if "fake news" in message.content.lower():
             msg = "*FAKE NEWS DETECTED. TERMINATING FAKE NEWS.*"
             await client.send_message(message.channel, msg)
 
-
         # If someone calls "!purge"
         if message.content.startswith("!purge"):
-            await client.purge_from(message.channel)
-
+            await client.purge_from(message.channel, limit=5)
 
         # If someone calls "!remindme"
         if message.content.startswith("!remindme"):
-
-            # Split the message into "!remindme", <minutes>, <message>
-            # Note: <message> could be multiple strings
-            command = message.content.split(" ")
-
-            error_msg = ""
-
-            # If valid number of args
-            if len(command) >= 3:
-
-                # Attempt to parse <minutes>
-                minutes = parse_minutes(command[1])
-
-                # If valid number of <minutes>
-                if 1 <= minutes <= 60:
-
-                    # TESTING: Post the data to be timed
-                    msg  = "Author: <{0.author.mention}>, ".format(message)
-                    msg += "Minutes: <%d>" % minutes
-                    msg += "Message: <%s>" % " ".join(command[2:])
-
-                    await client.send_message(message.channel, msg)
-
-                else:
-                    error_msg = "Invalid <minutes>; must be [1, 60]"
-
-            else:
-                error_msg = "Invalid number of commmands"
-
-            if error_msg is not "":
-                await client.send_message(message.channel, "Error: %s" % error_msg)
+            await remind_me(message)
 
 
-@client.event
-async def on_ready():
-    print("%s (ID: %s) logged in" % (client.user.name, client.user.id))
-    print("----------------------------------------")
+async def remind_me(message):
+    """
+    Splits a message into ["!remindme", <minutes>, <message>]
+    After <minutes>, mention the author and post <message>
+    Note: <message> could be multiple strings, so join() is used
+    """
 
-    # "general" chat channel id
-    general_id = "311344943111340033"
+    command = message.content.split(" ")
 
-    # Post a message when the bot comes online
-    msg = "*T-800 ONLINE. YOUR CLOTHES, GIVE THEM TO ME. NOW.*"
-    await client.send_message(client.get_channel(general_id), msg)
+    error_msg = ""
 
-    # Set the bot's "Playing" status
-    await client.change_presence(game=discord.Game(name="Human Simulator"))
+    # If valid number of args
+    if len(command) >= 3:
+
+        # Attempt to parse <minutes>
+        minutes = parse_minutes(command[1])
+
+        # If valid number of <minutes>
+        if 1 <= minutes <= 60:
+
+            # TESTING: Post the data to be timed
+            msg = "Author: <{0.author.mention}>, ".format(message)
+            msg += "Minutes: <%d>, " % minutes
+            msg += "Message: <%s>" % " ".join(command[2:])
+
+            await client.send_message(message.channel, msg)
+
+        else:
+            error_msg = "Invalid <minutes>; must be [1, 60]"
+
+    else:
+        error_msg = "Invalid number of commmands"
+
+    if error_msg is not "":
+        await client.send_message(message.channel, "Error: %s" % error_msg)
 
 
-# Attempts to parse an int from a string
 def parse_minutes(str_input):
+    """
+    Attempt to parse an int from a string
+    """
+
     minutes = -1
 
     try:
         minutes = int(str_input)
     except:
         None
-    
+
     return minutes
 
 
