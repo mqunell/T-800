04d11e64501213c6579e851f80c59e3acf3a475f
Matt <mqunell@gmail.com>
2019-11-07 11:56:06 -0800

    Fix pathing

--- src/launcher.py
+++ src/launcher.py
@@ -1,3 +1,9 @@
+import sys
+
+# Windows and AWS Ubuntu paths
+sys.path.append('C:\\Users\\mqune\\OneDrive\\Documents\\Code\\PycharmProjects\\T-800')
+#sys.path.append('/home/ubuntu/T-800')
+
 from src.bot import T800
 
 bot = T800()
